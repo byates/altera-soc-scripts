6edf39901d5feb812ba6fabc93d36a2991f3bc71
Brent Yates <brent.yates@gmail.com>
2015-09-06 21:03:23 -0400

    Modified the make_uboot_script script to take in the u-boot.script file as a parameter. It can now be used for multiple scripts.

--- make_uboot_script_from_text.sh
+++ make_uboot_script_from_text.sh
@@ -1,2 +1,6 @@
+if [ "$1" == "" ]; then
+  echo "Missing path to u-boot.script file."
+  exit -1
+fi
 PATH=../OfficalAlteraRepos/u-boot-socfpga/tools:$PATH
-mkimage  -A arm -O linux -T script -C none -a 0 -e 0 -n "u-boot for loading FPGA" -d u-boot-load-fpga.script u-boot.scr
+mkimage  -A arm -O linux -T script -C none -a 0 -e 0 -n "u-boot for loading FPGA" -d $1 u-boot.scr
