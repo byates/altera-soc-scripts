b0ba8477f4d24b4ed09183d157d35b5d6cfdd8af
Brent Yates <brent.yates@gmail.com>
2016-06-06 21:22:39 -0400

    Removed path dependency

--- make_uboot_script_from_text.sh
+++ make_uboot_script_from_text.sh
@@ -2,5 +2,4 @@ if [ "$1" == "" ]; then
   echo "Missing path to u-boot.script file."
   exit -1
 fi
-PATH=../OfficalAlteraRepos/u-boot-socfpga/tools:$PATH
 mkimage  -A arm -O linux -T script -C none -a 0 -e 0 -n "u-boot for loading FPGA" -d $1 u-boot.scr
